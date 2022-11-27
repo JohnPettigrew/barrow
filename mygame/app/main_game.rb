045bd6f442297609136c6b90e727861b752dc3c7
John Pettigrew <john@pettigrew.org.uk>
2022-11-27 12:28:10 +0000

    Refactor main_game

--- mygame/app/main_game.rb
+++ mygame/app/main_game.rb
@@ -9,20 +9,14 @@ class MainGame
   def initialize
     @game_in_progress = false
     @wait_counter = -1
-    @treasures = @inventory_lights = @inventory_axes = 0
-    @title_text_size = 10
     @screen_height = 720
     @screen_width = 1280
-    @status_area_width = 560
-    @map_width = @screen_width - @status_area_width
-    @map_horizontal_centre = @status_area_width + @map_width / 2
-    @map_vertical_centre = @screen_height / 2
     @map_location_size = 72
-    @player = Player.new(x: @map_horizontal_centre, y: @map_vertical_centre, location_size: @map_location_size, path: SPRITE_PATHS[:player])
+    @status_area_width = @screen_width - @screen_height
   end
 
   def tick
-    @game_in_progress ? play_game : welcome_screen
+    @game_in_progress ? new_game : welcome_screen
   end
 
   private
@@ -40,7 +34,7 @@ class MainGame
     def draw_welcome_screen
       @welcome_menu ||= Menu.new(entries: [ {value: 'Start game', selected: true}, {value: 'High scores'}, {value: 'Rules'}, {value: 'Quit'} ])
       @welcome_menu.render(args: @args, top: 420)
-      outputs.labels  << { x: @screen_width / 2, y: 500, text: 'Welcome to the barrow!', size_enum: @title_text_size, alignment_enum: 1 }
+      outputs.labels  << { x: @screen_width / 2, y: 500, text: 'Welcome to the barrow!', size_enum: 10, alignment_enum: 1 }
       logo_width = 128
       outputs.sprites << { x: @screen_width / 2 - logo_width / 2, y: 140, w: logo_width, h: 101, path: SPRITE_PATHS[:logo] }  
     end
@@ -61,8 +55,11 @@ class MainGame
       end
     end
 
-    def play_game
+    def new_game
+      half_map_size = @screen_height / 2
+      @player = Player.new(x: @status_area_width + half_map_size, y: half_map_size, location_size: @map_location_size, path: SPRITE_PATHS[:player])
       @map = Map.new(left: @status_area_width, map_location_size: @map_location_size)
+      @treasures = @inventory_lights = @inventory_axes = 0
       draw_status_area
       draw_map_area
       @game_in_progress = false if inputs.keyboard.key_down.escape
@@ -70,13 +67,12 @@ class MainGame
 
     def draw_status_area
       horizontal_centre = @status_area_width / 2
-      height = 720
-      outputs.solids << { x: 0, y: 0, w: @status_area_width, h: height, r: 200, g: 200 }
-      outputs.labels  << {x: horizontal_centre, y: height - 20, text: "Current score: #{@treasures}", size_enum: 5, alignment_enum: 1}
-      outputs.labels  << {x: horizontal_centre, y: height/2 + 50, text: "Inventory", size_enum: 5, alignment_enum: 1}
-      outputs.labels  << {x: horizontal_centre, y: height/2, text: "Lights: #{@inventory_lights}", size_enum: 3, alignment_enum: 1}
-      outputs.labels  << {x: horizontal_centre, y: height/2 - 40, text: "Pickaxes: #{@inventory_axes}", size_enum: 3, alignment_enum: 1}
-      outputs.labels  << {x: horizontal_centre, y: 50, text: "Hit 'Escape' to return to the menu", size_enum: 5, alignment_enum: 1}
+      outputs.solids << { x: 0, y: 0, w: @status_area_width, h: @screen_height, r: 200, g: 200 }
+      outputs.labels  << {x: horizontal_centre, y: @screen_height - 20, text: "Current score: #{@treasures}", size_enum: 5, alignment_enum: 1}
+      outputs.labels  << {x: horizontal_centre, y: @screen_height/2 + 50, text: "Inventory", size_enum: 5, alignment_enum: 1}
+      outputs.labels  << {x: horizontal_centre, y: @screen_height/2, text: "Lights: #{@inventory_lights}", size_enum: 3, alignment_enum: 1}
+      outputs.labels  << {x: horizontal_centre, y: @screen_height/2 - 40, text: "Pickaxes: #{@inventory_axes}", size_enum: 3, alignment_enum: 1}
+      outputs.labels  << {x: horizontal_centre, y: 50, text: "Hit 'Escape' to end your game", size_enum: 5, alignment_enum: 1}
     end
 
     def draw_map_area
